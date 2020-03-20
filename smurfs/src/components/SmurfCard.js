a673e7808c4384d196f450a0cbdf90b62d1d7e19
Alec Dye <alecdye@gmail.com>
2020-03-20 08:20:19 -0700

    Building component structure

---
+++ smurfs/src/components/SmurfCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SmurfCard = () => {
+  return <></>;
+};
+
+export default SmurfCard;
---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SmurfList = () => {
+  return <></>;
+};
+
+export default SmurfList;
